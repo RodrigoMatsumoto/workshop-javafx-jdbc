3260b1b23c28c2a533837498ae3208ff12514650
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-05-25 17:40:33 -0300

    Main view

--- src/main/java/org/example/workshopjavafxjdbc/application/Main.java
+++ src/main/java/org/example/workshopjavafxjdbc/application/Main.java
@@ -1,12 +1,27 @@
 package org.example.workshopjavafxjdbc.application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
   @Override
-  public void start(Stage stage) {
+  public void start(Stage primaryStage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/workshopjavafxjdbc/MainView.fxml"));
+    Parent parent = fxmlLoader.load();
+    Scene mainScene = new Scene(parent);
 
+    primaryStage.setScene(mainScene);
+    primaryStage.setTitle("Sample JavaFX application");
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch();
   }
 }
